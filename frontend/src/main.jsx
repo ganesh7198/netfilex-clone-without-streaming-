6dde2c83e75c342416369389326f1bbebd552b09
ganesh7198 <gg7198596@gmail.com>
2026-01-13 14:12:50 +0530

    everything is done need some minor improvement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+     <App />
   </BrowserRouter>
 );
